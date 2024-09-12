bfe8fb3fb1e0f51e29417a5158ac41c47907dae4
Ariel Marinho <arieldreis@gmail.com>
2024-09-11 21:54:28 -0300

    Initial commit

---
+++ EXERCÍCIOS DE HTML-CSS/EX.001/script.js
@@ -0,0 +1,30 @@
+<style>
+        body, html {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        /* Estilos para o círculo */
+        .container {
+            text-align: center;
+        }
+        .meu-circulo {
+            fill: green;
+            stroke: red;
+            stroke-width: 55;
+        }
+        .meu-circulo:hover {
+            fill: pink;
+            stroke-width: 60;
+        }
+		.meu-quadrado {
+            fill: purple;
+            stroke: red;
+            stroke-width: 100;
+        }
+        .meu-quadrado:hover {
+            fill: pink;
+            stroke-width: 100;
+    </style>
