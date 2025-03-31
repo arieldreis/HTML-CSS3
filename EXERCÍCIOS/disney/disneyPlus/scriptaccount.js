cf12329e175a69da0d8211fa3c76a8213cfafe87
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-03-31 10:14:31 -0300

    Mais arquivos

--- EXERCÍCIOS/disney/disneyPlus/scriptaccount.js
+++ EXERCÍCIOS/disney/disneyPlus/scriptaccount.js
@@ -1,21 +1,31 @@
 document.getElementById('email_form').addEventListener('submit', teste);
-function teste(event){
-    let erro = false;
-    erro = true;
-    if(erro) event.preventDefault();
+function teste(){
+    event.preventDefault();
+    let id = document.getElementById('ident');
     let e_mail = document.getElementById('e_mail');
     let senha = document.getElementById('password');
+    let nascimento = document.getElementById('nascimento');
+    let usuario = document.getElementById('nomeusuario');
+    let telefone = document.getElementById('telefone');
     let identificacao = e_mail.value.trim();
     let password = senha.value;
+    let birthday = nascimento.value;
+    let nameuser = usuario.value;
+    let phone = telefone.value;
+    let ident = id.value
     let res = document.getElementById('resultado');
     res.style.fontSize = "20px";
     // Limpar bordas vermelhas antes de verificar
     e_mail.style.border = "1px solid #ccc";
     senha.style.border = "1px solid #ccc";
-    if(identificacao === "" || password === ""){
+    if(identificacao === "" || password === "" || birthday === "" || nameuser === "" || phone === "" || ident === ""){
         res.textContent = "❗Este e-mail ou senha não tem o formato correto. Tentar novamente?";
         e_mail.style.border = "2px solid red";
         senha.style.border = "2px solid red";
+        id.style.border = "2px solid red";
+        nascimento.style.border = "2px solid red";
+        usuario.style.border = "2px solid red";
+        telefone.style.border = "2px solid red";
         e_mail.focus(); // Foco no campo email
     }
     else if(password.length > 64 || password.length < 10){
